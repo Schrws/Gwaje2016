32b26ad0e235699f280052cf92417099e03cab8a
Kevin Roh <suzynjun99@naver.com>
2016-12-27 21:45:54 +0900

    HTTPS

--- src/proj/Util.java
+++ src/proj/Util.java
@@ -2,16 +2,28 @@ package proj;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.HTTP;
 
 import java.io.BufferedReader;
+import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +31,10 @@ import java.util.List;
  * Created by SchrwsK on 2016-12-26.
  */
 public class Util {
-    public static DefaultHttpClient httpclient;
+    public static HttpClient httpclient;
     public static boolean login(String id, String passwd) {
         try {
-            if (httpclient == null) httpclient = new DefaultHttpClient();
+            if (httpclient == null) httpclient = getHttpClient();
             HttpResponse response;
             HttpEntity entity;
 
@@ -60,5 +72,29 @@ public class Util {
             result.append(line);
         rd.close();
         return result.toString();
+    }
+
+    private static HttpClient getHttpClient() { // http://blog.saltfactory.net/android/using-https-on-android.html
+        try {
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null, null);
+
+            SSLSocketFactory sf = new SFSSLSocketFactory(trustStore);
+            sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            HttpParams params = new BasicHttpParams();
+            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+            SchemeRegistry registry = new SchemeRegistry();
+            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+            registry.register(new Scheme("https", sf, 443));
+
+            ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+
+            return new DefaultHttpClient(ccm, params);
+        } catch (Exception e) {
+            return new DefaultHttpClient();
+        }
     }
 }
